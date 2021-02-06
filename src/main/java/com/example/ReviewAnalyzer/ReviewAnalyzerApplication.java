3f88763730ddc90d24af9e2401fcb479519fc7f9
MavSoccer1417 <MavSoccer1417@yahoo.com>
2021-02-06 17:17:40 +0000

    Initial

---
+++ src/main/java/com/example/ReviewAnalyzer/ReviewAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReviewAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReviewAnalyzer/ReviewAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReviewAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
